01f9f470707387f4b807b16be8573fdde9012541
David Torres <dftp93@gmail.com>
2020-03-12 00:50:09 -0500

    Added about me to the project

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
-console.log('Estable');
+document.addEventListener("DOMContentLoaded", function(event) {
+  
+  const page_me = document.getElementById("page_me");
+  const get_page_me = document.querySelectorAll("#get-page_me");
+  
+  for (let i = 0; i < get_page_me.length; i++) {
+    
+    get_page_me[i].addEventListener("click", function() {
+
+      // e.preventDefault();
+      page_me.classList.toggle('bottom-0-important');
+      console.log('Estable');
+  
+    });
+    
+  }
+
+});
